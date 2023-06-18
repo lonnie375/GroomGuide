3f5a981062839e0aba45081dba17a46499db0ca8
Lonnie Johnson <lonnie.e.johnson@gmail.com>
2023-06-18 05:58:27 -0500

    update

---
+++ Controllers/StylistsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroomGuide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StylistsController : ControllerBase
+    {
+    }
+}
