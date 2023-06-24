1e5b6077618739f033e9e06fd09e4826ca3cc274
Lonnie Johnson <lonnie.e.johnson@gmail.com>
2023-06-24 03:39:03 -0500

    update

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -16,9 +16,61 @@ namespace GroomGuide.Controllers
         {
             _context = context;
         }
+        
+        //Create Appointment
+        [HttpPost("CreateAppointment")]
+        public IActionResult CreateAppointment(Appointment appt)
+        {
+            _context.Appointments.Add(appt);
+            _context.SaveChanges();
+            return Ok();
+        }
 
+        //Get All Appointments
+        [HttpGet("GetStylistAppointments")]
+        public IActionResult GetAllAppointmentsByStylist(int id)
+        {
+            if (_context.Appointments == null)
+            {
+                return NotFound();
+            }
 
+            var response = _context.Appointments.Where(x => x.StylistId == id).ToList();
+            return Ok(response);
+        }
 
+        //Delete Appointment
+        [HttpDelete("DeleteAppointment")]
+        public IActionResult DeleteAppointment(int id)
+        {
+            Appointment appt = _context.Appointments.FirstOrDefault(x => x.Id == id);
+
+            if (appt == null)
+            {
+                return BadRequest(appt);
+            }
+
+            _context.Appointments.Remove(appt);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        //Edit Appointment 
+        [HttpPut("EditAppointment")]
+        public IActionResult EditAppointment(int id, Appointment appointment)
+        {
+            Appointment appt = _context.Appointments.FirstOrDefault(x => x.Id == id);
+
+            appt.Time = appointment.Time;
+            appt.ServiceId = appointment.ServiceId;
+            appt.Tip = appointment.Tip;
+            appt.ClientEmail = appointment.ClientEmail;
+            appt.AmountPaid = appointment.AmountPaid;
+
+            _context.Appointments.Update(appt);
+            _context.SaveChanges();
+            return Ok();
+        }
 
     }
 }
