36e91d23a421b0aaf8654b4a00bbd65c24d7015b
Lonnie Johnson <lonnie.e.johnson@gmail.com>
2023-09-05 18:57:09 -0500

    Created search feature for stylist

--- GroomFront/GroomFront/src/app/components/stylists/stylists.component.ts
+++ GroomFront/GroomFront/src/app/components/stylists/stylists.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Stylists } from '../../Interfaces/stylists';
 import { LocalApiService } from '../../Services/localapiservices.service';
 
@@ -11,21 +10,35 @@ import { LocalApiService } from '../../Services/localapiservices.service';
 export class StylistsComponent implements OnInit {
   
   stylists: Stylists[] | undefined;
+  searchResults: Stylists[] | undefined;
+  searchTerm: string = '';
 
   constructor(private localApiService: LocalApiService) { }
 
   ngOnInit(): void {
-     this.getAllStylists();
+    this.getAllStylists();
   }
 
   getAllStylists(): void {
     this.localApiService.getAllStylists().subscribe(
       (response: Stylists[]) => {
         this.stylists = response;
+        this.searchResults = response; // Initialize searchResults with all stylists
       },
       (error: any) => {
         console.error('Error getting stylists:', error);
       }
     );
+  }
+
+  searchStylists(): void {
+    // Filter stylists based on the searchTerm
+    if (this.searchTerm.trim() !== '') {
+      this.searchResults = this.stylists?.filter(stylist =>
+        stylist.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else {
+      this.searchResults = this.stylists; // If search term is empty, show all stylists
+    }
   }
 }
